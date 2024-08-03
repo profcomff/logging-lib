948d87587e578faa23cd27983f5a654b324e0a3e
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2024-08-04 01:02:21 +0300

    Autochange version on publish

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
+with open('README.md', 'r') as readme_file:
     readme = readme_file.read()
 
 setup(
-    name="logging_profcomff",
-    version="2023.10.29",
-    author="Semyon Grigoriev",
+    name='logging_profcomff',
+    version='1.0.0',  # Не менять, работает автоматика
+    author='Semyon Grigoriev',
     long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/profcomff/logging-lib",
+    long_description_content_type='text/markdown',
+    url='https://github.com/profcomff/logging-lib',
     packages=find_packages(),
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     classifiers=[
-        "Programming Language :: Python :: 3.11",
+        'Programming Language :: Python :: 3.11',
     ],
 )
